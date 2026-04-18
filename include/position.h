bb1c76468abca6a1592f1871e9e78a67e7ca04ef
lowlevelguy <virtual.code@protonmail.com>
2026-04-18 20:04:33 +0100

    Fixed a few typos. Changed distance type to uint16_t as that's the precision offered by the sensor. Refined the documentation for position_trilateration, and wrote the documentation for position_ols and position_fgls.

--- include/position.h
+++ include/position.h
@@ -1,24 +1,108 @@
 #pragma once
 
-extern const uint32_t ANCHORS[4][2];
+#include <stdint.h>
 
-/*
- * Performs trilateration to find the coordinates of the target given its
- * distances to three non-collinear anchor points.
- *
- * Algorithm Description:
- * Let E denote the matrix of edges, with (k-1)th row ANCHORS[0] - ANCHORS[k].
- * The target point x satisfies the equation Ex = y, and hence x = E^{-1}y,
- * where y is the vector defined as follows:
- *      y[k-1] = (dist[0]**2 - dist[k]**2 + norm_sq(a[k]) - norm_sq(a[0])) / 2
- * We use norm_sq(u) to denote the norm squared of a vector u, and a[k] as a
- * shorthand for ANCHORS[k].
- *
- * @param uint64_t* dist: array of distances from the target to the anchors
- * @param int64_t* pos: array that will contain the estimated position of the
- *                      target at the end of function execution.
+extern const double anchor_pos[4][2];
+
+/** @brief Performs trilateration to estimate the position of the target.
+  *
+  * @details
+  * Performs trilateration to find the coordinates of the target given its
+  * distances, provided in the buffer dist in mm, to three non-collinear anchor
+  * points.
+  *
+  * Algorithm Description:
+  * If E denotes the 2x2 matrix of edges, with (k-1)th row given by the vector
+  * operation (anchor_pos[0] - anchor_pos[k]), the target point x satisfies the
+  * equation Ex = y under ideal circumstances, where y is
+  * the 2x1 vector defined as follows:
+  *      y[k-1] = (dist[0]**2 - dist[k]**2 + norm_sq(a[k]) - norm_sq(a[0])) / 2
+  * We use norm_sq(u) to denote the norm squared of a vector u, and a[k] as a
+  * shorthand for anchor_pos[k].
+  * This property allows us to recover x when y is known, via the formula:
+  *      x = E^{-1} y
+  *
+  * @param dist: array containing the distances from the target to each anchor
+  * in mm.
+  * @param pos: array that will contain the estimated position of the target,
+  * each coordinate in mm.
+  * 
+  * @return
+  * Returns 0 on success, and -1 if E is non-invertible, meaning that the anchors
+  * are collinear.
 */
-void position_trilateration(uint64_t* dist, int64_t* pos);
+int position_trilateration(uint16_t* dist, double* pos);
 
-int position_ols(uint64_t* dist, uint64_t* pos);
-int position_fgls(uint64_t* dist, uint64_t* pos);
+/** @brief Performs linear ordinary least squares to estimate the position of
+  * the target.
+  *
+  * @details
+  * Performs linear ordinary least squares to find the coordinates of the target
+  * given its distances, provided in the buffer dist in mm, to four anchor
+  * points, three of which are non-collinear.
+  * 
+  * Algorithm Description:
+  * If E denotes the 3x2 matrix of edges, with (k-1)th row given by the vector
+  * operation (anchor_pos[0] - anchor_pos[k]), the target point x is the unique
+  * minimizer of norm_sq(Ex-y) under ideal circumstances, where y is the 3x1
+  * vector defined as follows:
+  *      y[k-1] = (dist[0]**2 - dist[k]**2 + norm_sq(a[k]) - norm_sq(a[0])) / 2
+  * We use norm_sq(u) to denote the norm squared of a vector u, and a[k] as a
+  * shorthand for anchor_pos[k].
+  * This property allows us to recover x when y is known, via the formula:
+  *      x = (E^T E)^{-1} E^T y
+  * 
+  * @param dist: array containing the distances from the target to each anchor
+  * in mm.
+  * @param pos: array that will contain the estimated position of the target,
+  * each coordinate in mm.
+  * 
+  * @return
+  * Returns 0 on success, and -1 if (E^T E) is non-invertible, meaning
+  * that the condition of three of the anchors being non-collinear is unfulfilled.
+  */
+int position_ols(uint16_t* dist, double* pos);
+
+/** @brief Performs linear feasible generalized least squares to estimate the
+  * position of the target.
+  *
+  * @details
+  * Performs linear feasile generalized least squares to estimate the position
+  * the target.
+  * 
+  * Algorithm Description:
+  * If E denotes the 3x2 matrix of edges, with (k-1)th row given by the vector
+  * operation (anchor_pos[0] - anchor_pos[k]), the best unbiased linear estimator
+  * hat{x} for the target x, assuming Gaussian i.i.d. noise on the distance
+  * measurements, is given by the following expression:
+  *      hat{x} = (E^T P^{-1} E)^{-1} E^T P^{-1} y
+  * where y is the 3x1 vector defined as follows:
+  *      y[k-1] = (dist[0]**2 - dist[k]**2 + norm_sq(a[k]) - norm_sq(a[0])) / 2
+  * and P is the covariance matrix of the vector y, given by:
+  *     P[i,j] = s**4 / 2  + (s * d[0])**2               (i != j)
+  *     P[i,i] = s**4 + s**2 * (d[0]**2 + d[i]**2)
+  * We use norm_sq(u) to denote the norm squared of a vector u, a[k] as a
+  * shorthand for anchor_pos[k]. Moreover, d[k] denotes the true distance
+  * between the target and anchor_pos[k], and s**2 the variance of the noise
+  * in the distance measurements.
+  * 
+  * Given that both d[k]**2 and s**2 are unknowns, we estimate them, and use
+  * their estimators to construct an estimator for P, which will be further
+  * used to construct an estimator for the target.
+  * The simplest estimator for d[k]**2, and the one which we use, is dist[k]**2.
+  * As for estimating s**2, we first invoke position_ols() to obtain an estimate
+  * \tilde{x} on the target's position. Our estimator \hat{s}**2 for s**2 is
+  * then given by:
+  *     \hat{s}**2 = \sum{(dist[k] - norm(\tilde{x} - anchor_pos[k]))**2} / 2
+  * where we use norm(u) to denote the norm of the vector u.
+  * 
+  * @param dist: array containing the distances from the target to each anchor
+  * in mm.
+  * @param pos: array that will contain the estimated position of the target,
+  * each coordinate in mm.
+  * 
+  * @return
+  * Returns 0 on success, and -1 if (E^T P^{-1} E) is non-invertible, meaning
+  * that the condition of three of the anchors being non-collinear is unfulfilled.
+  */
+int position_fgls(uint16_t* dist, double* pos);
--- src/position.cpp
+++ src/position.cpp
@@ -31,7 +31,7 @@ void matmat_mult(const double* mat1, const uint8_t mat1_rows, const uint8_t mat1
 }
 
 int mat2_inv(const double* mat, double* output) {
-    const double* det = mat[0]*mat[3] - mat[1]*mat[2];
+    const double det = mat[0]*mat[3] - mat[1]*mat[2];
     if (det < EPS && det > -EPS)
         return -1;
 
@@ -45,23 +45,27 @@ int mat2_inv(const double* mat, double* output) {
 
 /********** LIBRARY FUNCTIONS **********/
 
-void position_trilateration(uint64_t* dist, int32_t* pos) {
+int position_trilateration(uint16_t* dist, double* pos) {
     // Use the matrix of cofactors to compute the inverse of the edges matrix E
-    static const int64_t edges_det =
-        (ANCHORS[0][0] - ANCHORS[1][0])*(ANCHORS[0][1] - ANCHORS[2][1])
-        - (ANCHORS[0][0] - ANCHORS[2][0])*(ANCHORS[0][1] - ANCHORS[1][1]);
+    static const double edges_det =
+        (anchor_pos[0][0] - anchor_pos[1][0])*(anchor_pos[0][1] - anchor_pos[2][1])
+        - (anchor_pos[0][0] - anchor_pos[2][0])*(anchor_pos[0][1] - anchor_pos[1][1]);
+
+    if (edges_det < EPS && edges_det > -EPS)
+        return -1;
+    
     static const double edges_inv[] = {
-        (double)(ANCHORS[0][1] - ANCHORS[2][1]) / det,
-        -(double)(ANCHORS[0][1] - ANCHORS[1][1]) / det,
-        -(double)(ANCHORS[0][0] - ANCHORS[2][0]) / det,
-        (double)(ANCHORS[0][0] - ANCHORS[1][0]) / det
+        (anchor_pos[0][1] - anchor_pos[2][1]) / edges_det,
+        -(anchor_pos[0][1] - anchor_pos[1][1]) / edges_det,
+        -(anchor_pos[0][0] - anchor_pos[2][0]) / edges_det,
+        (anchor_pos[0][0] - anchor_pos[1][0]) / edges_det
     };
 
     // Compute the vector of coefficients y
     static const double norms_sq[] = {
-        ANCHORS[0][0]*ANCHORS[0][0] + ANCHORS[0][1]*ANCHORS[0][1],
-        ANCHORS[1][0]*ANCHORS[1][0] + ANCHORS[1][1]*ANCHORS[1][1],
-        ANCHORS[2][0]*ANCHORS[2][0] + ANCHORS[2][1]*ANCHORS[2][1]
+        anchor_pos[0][0]*anchor_pos[0][0] + anchor_pos[0][1]*anchor_pos[0][1],
+        anchor_pos[1][0]*anchor_pos[1][0] + anchor_pos[1][1]*anchor_pos[1][1],
+        anchor_pos[2][0]*anchor_pos[2][0] + anchor_pos[2][1]*anchor_pos[2][1]
     };
     const double coeffs[] = {
         (dist[0]*dist[0] - dist[1]*dist[1] + norms_sq[1] - norms_sq[0]) / 2,
@@ -70,23 +74,24 @@ void position_trilateration(uint64_t* dist, int32_t* pos) {
 
     // Estimate the target as E^{1}y
     matvec_mult(edges_inv, 2, 2, coeffs, pos);
+    return 0;
 }
 
-int position_ols(uint64_t* dist, uint64_t* pos) {
+int position_ols(uint16_t* dist, double* pos) {
     static const double edges[] = {
-        ANCHORS[0][0] - ANCHORS[1][0],
-        ANCHORS[0][1] - ANCHORS[1][1],
-        ANCHORS[0][0] - ANCHORS[2][0],
-        ANCHORS[0][1] - ANCHORS[2][1],
-        ANCHORS[0][0] - ANCHORS[3][0],
-        ANCHORS[0][1] - ANCHORS[3][1],
+        anchor_pos[0][0] - anchor_pos[1][0],
+        anchor_pos[0][1] - anchor_pos[1][1],
+        anchor_pos[0][0] - anchor_pos[2][0],
+        anchor_pos[0][1] - anchor_pos[2][1],
+        anchor_pos[0][0] - anchor_pos[3][0],
+        anchor_pos[0][1] - anchor_pos[3][1],
     }, edges_transpose[] = {
-        ANCHORS[0][0] - ANCHORS[1][0],
-        ANCHORS[0][0] - ANCHORS[2][0],
-        ANCHORS[0][0] - ANCHORS[3][0],
-        ANCHORS[0][1] - ANCHORS[1][1],
-        ANCHORS[0][1] - ANCHORS[2][1],
-        ANCHORS[0][1] - ANCHORS[3][1],
+        anchor_pos[0][0] - anchor_pos[1][0],
+        anchor_pos[0][0] - anchor_pos[2][0],
+        anchor_pos[0][0] - anchor_pos[3][0],
+        anchor_pos[0][1] - anchor_pos[1][1],
+        anchor_pos[0][1] - anchor_pos[2][1],
+        anchor_pos[0][1] - anchor_pos[3][1],
     };
 
     // Compute the necessary matrices
@@ -101,10 +106,10 @@ int position_ols(uint64_t* dist, uint64_t* pos) {
 
     // Compute the vectors of coefficients
     static const double norms_sq[] = {
-        ANCHORS[0][0]*ANCHORS[0][0] + ANCHORS[0][1]*ANCHORS[0][1],
-        ANCHORS[1][0]*ANCHORS[1][0] + ANCHORS[1][1]*ANCHORS[1][1],
-        ANCHORS[2][0]*ANCHORS[2][0] + ANCHORS[2][1]*ANCHORS[2][1],
-        ANCHORS[3][0]*ANCHORS[3][0] + ANCHORS[3][1]*ANCHORS[3][1],
+        anchor_pos[0][0]*anchor_pos[0][0] + anchor_pos[0][1]*anchor_pos[0][1],
+        anchor_pos[1][0]*anchor_pos[1][0] + anchor_pos[1][1]*anchor_pos[1][1],
+        anchor_pos[2][0]*anchor_pos[2][0] + anchor_pos[2][1]*anchor_pos[2][1],
+        anchor_pos[3][0]*anchor_pos[3][0] + anchor_pos[3][1]*anchor_pos[3][1],
     };
     const double coeffs[] = {
         (dist[0]*dist[0] - dist[1]*dist[1] + norms_sq[1] - norms_sq[0]) / 2,
@@ -113,9 +118,10 @@ int position_ols(uint64_t* dist, uint64_t* pos) {
     };
 
     // Estimate the target as (E^T E)^{-1} E^T y
-    matvec_mult(full_matrx, 2, 3, coeffs, pos);
+    matvec_mult(full_matrix, 2, 3, coeffs, pos);
+    return 0;
 }
 
-void position_fgls(uint64_t* dist, uint64_t* pos) {
+void position_fgls(uint16_t* dist, uint64_t* pos) {
 
 }
