ef786ff8c6c0ee5d6c6335d3415c6dfdee36c4a9
EladabYoussef <youssefladeb@gmail.com>
2026-04-30 12:27:45 +0100

    supporting hw serial and sw serial in sensors code

--- include/sensor.h
+++ include/sensor.h
@@ -2,11 +2,24 @@
 
 #include <stdint.h>
 #include <Arduino.h>
+#include <SoftwareSerial.h>
+#include <HardwareSerial.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
 #define MAX_WAIT_TIME_MS 100
 #define SENSOR_TRIGGER_BYTE 0x01
 #define MAX_SENSORS 4
 
-extern HardwareSerial *SENSORS[MAX_SENSORS];
+typedef struct SerialHandle {
+    enum { HW, SW } type;
+    union {
+        HardwareSerial* hw;
+        SoftwareSerial* sw;
+    };
+} SerialHandle_t;
 
-int init_sensor(int sensor_index, int uart_index, int baud_rate, int RX, int TX);
+extern SerialHandle_t *SENSORS[MAX_SENSORS];
+
+int init_sensor(int sensor_index, SerialHandle_t* handle, int baud_rate, int RX, int TX);
 int32_t read_sensor(int sensor_index);
--- src/sensor.cpp
+++ src/sensor.cpp
@@ -1,76 +1,77 @@
 #include "sensor.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-HardwareSerial *SENSORS[MAX_SENSORS];
 
-int init_sensor(int sensor_index, int uart_index, int baud_rate, int RX, int TX) {
-    //input validation
-    if(sensor_index < 0 || sensor_index >= MAX_SENSORS) {
-        return 0;
-    }
+SerialHandle_t* SENSORS[MAX_SENSORS] = {NULL, NULL, NULL, NULL};
 
-    if(SENSORS[sensor_index] != NULL) {
-        delete SENSORS[sensor_index];
-        SENSORS[sensor_index] = NULL;
-    }
-
-    SENSORS[sensor_index] = new HardwareSerial(uart_index);
-    // initialize the sensor at the given index 
-    //with the specified baud rate and RX/TX pins
-
-    SENSORS[sensor_index]->begin(baud_rate, SERIAL_8N1, RX, TX);
-    return 1;
+static int sensor_available(SerialHandle_t* s) {
+    return (s->type == SerialHandle_t::HW)
+        ? s->hw->available()
+        : s->sw->available();
 }
 
+static int sensor_read(SerialHandle_t* s) {
+    return (s->type == SerialHandle_t::HW)
+        ? s->hw->read()
+        : s->sw->read();
+}
+
+static void sensor_write(SerialHandle_t* s, uint8_t data) {
+    if (s->type == SerialHandle_t::HW)
+        s->hw->write(data);
+    else
+        s->sw->write(data);
+}
+
+static void sensor_read_bytes(SerialHandle_t* s, uint8_t* buffer, size_t len) {
+    if (s->type == SerialHandle_t::HW)
+        s->hw->readBytes(buffer, len);
+    else
+        s->sw->readBytes(buffer, len);
+}
+
+int init_sensor(int sensor_index, SerialHandle_t* handle, int baud_rate, int RX, int TX) {
+    if (sensor_index < 0 || sensor_index >= MAX_SENSORS) return -1;
+    if (handle == NULL)                                   return -1;
+
+    SENSORS[sensor_index] = handle;
+
+    if (handle->type == SerialHandle_t::HW)
+        handle->hw->begin(baud_rate, SERIAL_8N1, RX, TX);
+    else
+        handle->sw->begin(baud_rate); // RX/TX already set in constructor in main
+
+    return 0;
+}
 
 //triggers the sensor and reads the distance
 int32_t read_sensor(int sensor_index) {
-    //input validation 
-    if (sensor_index < 0 || sensor_index >= MAX_SENSORS) {
-        return -1; 
-    }
+    if (sensor_index < 0 || sensor_index >= MAX_SENSORS) return -1;
+    if (SENSORS[sensor_index] == NULL)                   return -1;
 
-    //initialize buffer for sensor reading
-    uint8_t buffer[4];
-    if(SENSORS[sensor_index] == NULL) {
-        return -1;
-    }
+    SerialHandle_t* s = SENSORS[sensor_index];
 
-    //trigger the sensor
-    //according to the sheet of the sensor model AJ-SR04M, you have to send
-    //the value 0x01 to its uart port to trigger it to send its measurement
-    while (SENSORS[sensor_index]->available()) {
-        SENSORS[sensor_index]->read();
-    }
-    SENSORS[sensor_index]->write(SENSOR_TRIGGER_BYTE);
+    // flush stale data before triggering
+    while (sensor_available(s))
+        sensor_read(s);
 
-    //sensor has multiple modes of operation, we chose to use the mode 4
-    // in this mode , when triggered the sensor sends 32 bits of data
-    // in the format 0xff + H_byte + L_byte + checksum
-    // where H_byte and L_byte are the high and low bytes of the sensor reading
-    // and checksum is the lower 8 bits of the sum of H_byte and L_byte
+    sensor_write(s, SENSOR_TRIGGER_BYTE);
+
+    // wait for 4 bytes: 0xFF + H_byte + L_byte + checksum
     unsigned long start = millis();
-    while (SENSORS[sensor_index]->available() < 4) {
-        if((millis() - start) > MAX_WAIT_TIME_MS) {
-            return -1;
-        }
-        vTaskDelay(1);
+    while (sensor_available(s) < 4) {
+        if ((millis() - start) > MAX_WAIT_TIME_MS) return -1;
+        vTaskDelay(1); // yield to FreeRTOS scheduler while waiting
     }
-    
-    SENSORS[sensor_index]->readBytes(buffer, 4);
 
-    if(buffer[0] == 0xFF) {
-        uint8_t high_byte = buffer[1];
-        uint8_t low_byte = buffer[2];
+    uint8_t buffer[4];
+    sensor_read_bytes(s, buffer, 4);
 
-        uint8_t checksum = (uint8_t)(((uint16_t)low_byte + high_byte) & 0xFF);
-        
-        if(checksum != buffer[3]) {
-            return -1;
-        }
-        int32_t distance = (int32_t)((high_byte << 8) | low_byte);
-        return distance; 
-    
-    }
-    return -1;
+    if (buffer[0] != 0xFF) return -1;
+
+    uint8_t high_byte = buffer[1];
+    uint8_t low_byte  = buffer[2];
+    uint8_t checksum  = (uint8_t)(((uint16_t)high_byte + low_byte) & 0xFF);
+
+    if (checksum != buffer[3]) return -1;
+
+    return (int32_t)((high_byte << 8) | low_byte);
 }
