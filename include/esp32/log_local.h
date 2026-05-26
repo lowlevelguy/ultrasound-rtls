251ef07ebf7bb047566786f8c69819123322bd0d
EladabYoussef <youssefladeb@gmail.com>
2026-05-26 14:50:47 +0100

    created the log task and the on_read function that adds to the queue

--- include/esp32/log_local.h
+++ include/esp32/log_local.h
@@ -1,8 +1,11 @@
 #pragma once
 #include "FS.h"
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 
 #define LOG_FILE_PATH "/log.csv"
 #define QUEUE_SIZE 10
+TaskHandle_t logger_handle = NULL;
 
 typedef struct logQueueEntry {
     float x;
@@ -10,6 +13,6 @@ typedef struct logQueueEntry {
     unsigned long timestamp;
 } log_queue_entry_t;
 
-int initialize_log_file(fs::FS &fs);
-int save_to_queue(const float* pos, const unsigned long timestamp);
-int save_to_file(fs::FS &fs);
+int initialize_log_file(fs::FS *fs);
+int on_read(const float* pos, const unsigned long timestamp);
+int save_to_file(fs::FS *fs, uint32_t buffer_index);
--- src/esp32/log_local.cpp
+++ src/esp32/log_local.cpp
@@ -1,12 +1,14 @@
 #include "esp32/log_local.h"
 #include <LittleFS.h>
 
-static log_queue_entry_t log_queue[QUEUE_SIZE];
-static int queue_index = 0;
 
-int initialize_log_file(fs::FS &fs) {
-    if(!fs.exists(LOG_FILE_PATH)) {
-        File file = fs.open(LOG_FILE_PATH, FILE_WRITE);
+static log_queue_entry_t log_queue[2][QUEUE_SIZE];
+static uint32_t queue_index = 0;
+static uint32_t buffer_index = 0;
+
+int initialize_log_file(fs::FS *fs) {
+    if(!fs->exists(LOG_FILE_PATH)) {
+        File file = fs->open(LOG_FILE_PATH, FILE_WRITE);
         if(!file) {
             return -1;
         }
@@ -17,16 +19,20 @@ int initialize_log_file(fs::FS &fs) {
 }
 
 // saves the contents of the queue to the log file, returns 0 on success and -1 on failure
-int save_to_file(fs::FS &fs) {
+int save_to_file(fs::FS *fs, uint32_t buffer_index) {
 
-    File file = fs.open(LOG_FILE_PATH, FILE_APPEND);
+    File file = fs->open(LOG_FILE_PATH, FILE_APPEND);
 
     if(!file) {
         return -1;
     }
 
     for(int i = 0; i < queue_index; ++i) {
-        if(!file.printf("%lu,%.2f,%.2f\n",log_queue[i].timestamp , log_queue[i].x, log_queue[i].y)) {
+        if(!file.printf("%lu,%.2f,%.2f\n",
+            log_queue[buffer_index][i].timestamp, 
+            log_queue[buffer_index][i].x, 
+            log_queue[buffer_index][i].y)) 
+        {
             file.close();
             return -1;
         }
@@ -37,15 +43,24 @@ int save_to_file(fs::FS &fs) {
 }
 
 //saves the position to the queue, returns 0 on success and 1 if the queue is full
-int save_to_queue(const float* pos, const unsigned long timestamp) {
+int on_read(const float* pos, const unsigned long timestamp) {
+    log_queue[buffer_index][queue_index].x = pos[0];
+    log_queue[buffer_index][queue_index].y = pos[1];
+    log_queue[buffer_index][queue_index].timestamp = timestamp;
+    queue_index++;
+
     if(queue_index >= QUEUE_SIZE) {
+        buffer_index = 1 - buffer_index;
+        xTaskNotify(logger_handle, 1-buffer_index, eSetValueWithOverwrite);
         return 1;
     }
 
-    log_queue[queue_index].x = pos[0];
-    log_queue[queue_index].y = pos[1];
-    log_queue[queue_index].timestamp = timestamp;
-    queue_index++;
-
     return 0;
 }
+
+void logger_task(void* file_s) {
+    uint32_t full_buff_idx;
+    xTaskNotifyWait(0, 0, &full_buff_idx, portMAX_DELAY);
+    save_to_file((fs::FS*)file_s, full_buff_idx);
+}
+
