1efb0caee4cca27c3aff0592200642832d0fed97
EladabYoussef <youssefladeb@gmail.com>
2026-05-29 12:11:02 +0100

    solved few bugs

--- src/esp32/log_local.cpp
+++ src/esp32/log_local.cpp
@@ -1,11 +1,11 @@
 #include "esp32/log_local.h"
 #include <LittleFS.h>
 #include "esp32/config.h"
-#include <stdatomic.h>
+#include <atomic>
 
 static log_queue_entry_t log_queue[2][QUEUE_SIZE];
 static uint32_t queue_index = 0;
-static _Atomic uint32_t buffer_index = 0;
+static std::atomic<uint32_t> buffer_index = 0;
 SemaphoreHandle_t buffer_mutex = NULL;
 TaskHandle_t logger_handle = NULL;
 static uint32_t bad_pkts_counter = 0;
@@ -23,7 +23,8 @@ int initialize_log_file(fs::FS *file_s) {
 }
 
 // saves the contents of the queue to the log file, returns 0 on success and -1 on failure
-static int save_to_file(fs::FS *file_s, uint32_t buffer_index) {
+static int save_to_file(fs::FS *file_s, uint32_t buff_idx) {
+
     File file = file_s->open(LOG_FILE_PATH, FILE_APPEND);
 
     if(!file) {
@@ -32,9 +33,9 @@ static int save_to_file(fs::FS *file_s, uint32_t buffer_index) {
 
     for(int i = 0; i < QUEUE_SIZE; ++i) {
         if(!file.printf("%lu,%.2f,%.2f\n",
-            log_queue[buffer_index][i].timestamp, 
-            log_queue[buffer_index][i].x, 
-            log_queue[buffer_index][i].y)) 
+            log_queue[buff_idx][i].timestamp, 
+            log_queue[buff_idx][i].x, 
+            log_queue[buff_idx][i].y)) 
         {
             file.close();
             return -1;
@@ -54,14 +55,16 @@ static uint8_t checksum8(uint8_t* buf, uint8_t buf_sz) {
 
 //saves the position to the queue, sends notification to logger task if queue is full
 static void on_read(const float* pos, const uint32_t timestamp) {
-    log_queue[buffer_index][queue_index].x = pos[0];
-    log_queue[buffer_index][queue_index].y = pos[1];
-    log_queue[buffer_index][queue_index].timestamp = timestamp;
+    uint32_t buff_idx = buffer_index.load();
+    log_queue[buff_idx][queue_index].x = pos[0];
+    log_queue[buff_idx][queue_index].y = pos[1];
+    log_queue[buff_idx][queue_index].timestamp = timestamp;
     queue_index++;
 
     if(queue_index >= QUEUE_SIZE) {
-        atomic_fetch_xor(&buffer_index, 1);
-        xTaskNotify(logger_handle, 1-buffer_index, eSetValueWithOverwrite);
+        uint32_t full_buff_index = buffer_index.load();
+        buffer_index.fetch_xor(1);
+        xTaskNotify(logger_handle, full_buff_index, eSetValueWithOverwrite);
         queue_index = 0;
     }
 }
